9bac95810567ed4388414ad83e72d1e5b2b590e7
Tilman Miraß <>
2020-10-23 11:21:09 +0200

    move state logic

--- src/pages/templates/diskursintervention/index.jsx
+++ src/pages/templates/diskursintervention/index.jsx
@@ -1,14 +1,27 @@
+import React, { useRef, useState } from "react";
+
 import Cockpit from "../../../templates/diskursintervention/cockpit";
-import React from "react";
-import { StoreProvider } from "../../../lib/store";
 import Template from "../../../templates/diskursintervention/template";
 
 export default () => {
+  const [state, setState] = useState({
+    categoryText: "Kategorie",
+    bodyText: "Lorem Ipsum is simply!!",
+    bodyTextScale: 100,
+    bodyTextScaleRange: [60, 150],
+    colorCombination: "darkGray_turquoise",
+    templateRef: useRef(null),
+    templateScale: true,
+  });
+
   return (
-    <StoreProvider>
-      <div className="container grid-12">
-        <Template />
+    <div className="container grid-12">
+      <div className="col-span-6">
+        <Template state={state} />
       </div>
-    </StoreProvider>
+      <div className="col-span-4 col-start-8">
+        <Cockpit state={state} setState={setState} />
+      </div>
+    </div>
   );
 };
