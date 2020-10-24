2b06a29c87db09cbbd6df5a81deac1e4e286a47d
Tilman Miraß <>
2020-10-24 18:52:22 +0200

    select default

--- src/components/colorCombinationsSelect.jsx
+++ src/components/colorCombinationsSelect.jsx
@@ -6,6 +6,7 @@ export default ({ state, setState }) => (
     // eslint-disable-next-line jsx-a11y/no-onchange
     onChange={(e) => setState({ ...state, colorCombination: e.target.value })}
     className="border-2 border-black"
+    value={state.colorCombination}
   >
     {colorCombinations.map((colorCombination) => (
       <option label={colorCombination.name}>{colorCombination.label}</option>
