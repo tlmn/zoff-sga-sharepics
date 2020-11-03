f268b78010a1420c496c3aa76624bb0de664fdfd
Tilman Miraß <mail@tlmn.io>
2020-11-03 12:59:57 +0100

    change default image

--- src/templates/selbstrepraesentation/slide-1/template.jsx
+++ src/templates/selbstrepraesentation/slide-1/template.jsx
@@ -48,7 +48,7 @@ export default ({ state, setState, thumbnail = false }) => {
                 backgroundImage: `url(${
                   state.slides[1].data.image.url !== null
                     ? state.slides[1].data.image.url
-                    : "/assets/images/olaf-scholz.jpg"
+                    : "/assets/images/selbstrepraesentation.jpg"
                 })`,
                 height: "100%",
                 backgroundPositionX: `${state.slides[1].data.image.position.x}px`,
