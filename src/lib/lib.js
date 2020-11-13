678d801c9a5cc5fdb1f9b2f2bdc44ee548bcc72f
Tilman Miraß <mail@tlmn.io>
2020-11-13 17:53:36 +0100

    fix html2image bug

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,24 +1,22 @@
 import { colorThemes, colors } from "../config/vars";
 
 import emojiRegex from "emoji-regex";
-import htmlToImage from "html-to-image";
 import slugify from "react-slugify";
+import { toJpeg } from "html-to-image";
 
 export const html2image = async ({ state, setState }, fileName = "solid") => {
   setState({ ...state, templateScale: false });
-  htmlToImage
-    .toJpeg(state.slides[state.currentSlide].ref.current, {
-      quality: 1,
-      width: 1080,
-      height: 1080,
-    })
-    .then(function (dataUrl) {
-      var link = document.createElement("a");
-      link.download = `sharepic-${slugify(fileName.substring(0, 20))}.jpg`;
-      link.href = dataUrl;
-      link.click();
-      setState({ ...state, templateScale: true });
-    });
+  toJpeg(state.slides[state.currentSlide].ref.current, {
+    quality: 1,
+    width: 1080,
+    height: 1080,
+  }).then(function (dataUrl) {
+    var link = document.createElement("a");
+    link.download = `sharepic-${slugify(fileName.substring(0, 20))}.jpg`;
+    link.href = dataUrl;
+    link.click();
+    setState({ ...state, templateScale: true });
+  });
 };
 export const formatEmojis = (text = "") => {
   return text.replace(
