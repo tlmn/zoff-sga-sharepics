e05198f0aefd894b984f855adea82079e66312f6
Tilman Miraß <mail@tlmn.io>
2020-11-18 09:44:26 +0100

    use input components

--- src/templates/mitglieder/slide-2/controlsRight.jsx
+++ src/templates/mitglieder/slide-2/controlsRight.jsx
@@ -1,86 +1,41 @@
+import DownloadButton from "../../../components/inputs/downloadButton";
 import React from "react";
-import { html2image } from "../../../lib/lib";
+import TextScale from "../../../components/inputs/textScale";
+import Textarea from "../../../components/inputs/textarea";
 
-export default ({ state, setState }) => (
-  <>
-    <label>Text</label>
-    <textarea
-      onChange={(e) =>
-        setState({
-          ...state,
-          ...state.slides.splice(2, 1, {
-            ...state.slides[2],
-            data: {
-              ...state.slides[2].data,
-              body: {
-                ...state.slides[2].data.body,
-                content: e.target.value,
-              },
-            },
-          }),
-        })
-      }
-      value={state.slides[2].data.body.content}
-      className="border-1"
-      rows={5}
-      cols={60}
-    />
+export default ({ state, setState }) => {
+  const currentSlide = 2;
+  return (
+    <>
+      <Textarea
+        label="Text"
+        state={state}
+        setState={setState}
+        propertyPath={`slides[${currentSlide}].data.body.content`}
+        rows={5}
+        cols={60}
+      />
 
-    <label>Textgröße</label>
-    <input
-      type="range"
-      id="bodyTextScale"
-      name="bodyTextScale"
-      min={state.slides[2].data.body.scaleRange[0]}
-      max={state.slides[2].data.body.scaleRange[2]}
-      value={state.slides[2].data.body.scale}
-      onChange={(e) =>
-        setState({
-          ...state,
-          ...state.slides.splice(2, 1, {
-            ...state.slides[2],
-            data: {
-              ...state.slides[2].data,
-              body: {
-                ...state.slides[2].data.body,
-                scale: e.target.value,
-              },
-            },
-          }),
-        })
-      }
-    />
+      <TextScale
+        state={state}
+        setState={setState}
+        propertyPath={`slides[[${currentSlide}].data.body.scale`}
+      />
 
-    <label>Lokalgruppe (optional)</label>
-    <textarea
-      onChange={(e) =>
-        setState({
-          ...state,
-          ...state.slides.splice(2, 1, {
-            ...state.slides[2],
-            data: {
-              ...state.slides[2].data,
-              localBranch: { content: e.target.value },
-            },
-          }),
-        })
-      }
-    >
-      {state.slides[2].data.localBranch.content}
-    </textarea>
-    <button
-      className="btn btn-download"
-      onClick={() =>
-        html2image(
-          {
-            state,
-            setState,
-          },
-          `mitglieder-3-${state.slides[2].data.body.content}`
-        )
-      }
-    >
-      Download
-    </button>
-  </>
-);
+      <Textarea
+        label="Text"
+        state={state}
+        setState={setState}
+        propertyPath={`slides[${currentSlide}].data.localBranch.content`}
+        rows={2}
+        cols={60}
+      />
+
+      <DownloadButton
+        state={state}
+        setState={setState}
+        fileNamePath={`slides[${currentSlide}].data.body.content`}
+      />
+    </>
+  );
+};
