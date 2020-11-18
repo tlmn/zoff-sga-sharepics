74317980c336e89ef0e95b415d77410b55c29252
Tilman Miraß <mail@tlmn.io>
2020-11-18 11:50:51 +0100

    use updateProperty function and ids for labels

--- src/components/inputs/bgImage.jsx
+++ src/components/inputs/bgImage.jsx
@@ -1,72 +1,50 @@
 import IconReset from "../../assets/svg/reset";
 import React from "react";
+import { updateProperty } from "../../lib/lib";
 
 export default ({ state, setState, currentSlide }) => (
   <>
-    <label>Hintergrundbild</label>
+    <label htmlFor="bgImage__fileName">Hintergrundbild</label>
     <input
       type="file"
-      id="file"
+      id="bgImage__fileName"
       name="file"
       onChange={(e) =>
         e.target.files[0] !== null &&
-        setState({
-          ...state,
-          ...state.slides.splice(currentSlide, 1, {
-            ...state.slides[currentSlide],
-            data: {
-              ...state.slides[currentSlide].data,
-              image: {
-                ...state.slides[currentSlide].data.image,
-                url: URL.createObjectURL(e.target.files[0]),
-              },
-            },
-          }),
-        })
+        updateProperty(
+          { state, setState },
+          `slides[${currentSlide}].data.image.url`,
+          URL.createObjectURL(e.target.files[0])
+        )
       }
     />
-    <label>Zoomfaktor für Bild</label>
+    <label htmlFor="bgImage__scale">Zoomfaktor für Bild</label>
     <input
       type="range"
-      id="imageScale"
+      id="bgImage__scale"
       name="imageScale"
       min="0"
       defaultValue={state.slides[currentSlide].data.image.scale}
       max="30"
       onChange={(e) =>
-        setState({
-          ...state,
-          ...state.slides.splice(currentSlide, 1, {
-            ...state.slides[currentSlide],
-            data: {
-              ...state.slides[currentSlide].data,
-              image: {
-                ...state.slides[currentSlide].data.image,
-                scale: e.target.value,
-              },
-            },
-          }),
-        })
+        updateProperty(
+          { state, setState },
+          `slides[${currentSlide}].data.image.scale`,
+          e.target.value
+        )
       }
     />
 
     <button
       className="btn flex justify-center mb-2"
       onClick={() =>
-        setState({
-          ...state,
-          ...state.slides.splice(currentSlide, 1, {
-            ...state.slides[currentSlide],
-            data: {
-              ...state.slides[currentSlide].data,
-              image: {
-                ...state.slides[currentSlide].data.image,
-                position: { x: 0, y: 0 },
-              },
-            },
-          }),
-        })
+        updateProperty(
+          { state, setState },
+          `slides[${currentSlide}].data.image.position`,
+          { x: 0, y: 0 }
+        )
       }
+      type="button"
     >
       <IconReset height="20" className="block mr-1" /> Bildausschnitt
     </button>
