dc727a13a86c044cae9e2d855b0dc2960d15f81c
Tilman Miraß <mail@tlmn.io>
2020-11-18 09:35:43 +0100

    use input components

--- src/templates/mitglieder/slide-0/controlsRight.jsx
+++ src/templates/mitglieder/slide-0/controlsRight.jsx
@@ -1,44 +1,26 @@
 import BgImage from "../../../components/inputs/bgImage";
-import ColorThemesSelect from "../../../components/inputs/colorThemesSelect";
+import DownloadButton from "../../../components/inputs/downloadButton";
+import Input from "../../../components/inputs/input";
 import React from "react";
-import { html2image } from "../../../lib/lib";
 
-export default ({ state, setState }) => (
-  <>
-    <BgImage state={state} setState={setState} currentSlide={0}/>
+export default ({ state, setState }) => {
+  const currentSlide = 0;
+  return (
+    <>
+      <BgImage state={state} setState={setState} currentSlide={currentSlide} />
 
-    <label>Text</label>
-    <input
-      onChange={(e) =>
-        setState({
-          ...state,
-          ...state.slides.splice(0, 1, {
-            ...state.slides[0],
-            data: {
-              ...state.slides[0].data,
-              statement: {
-                content: e.target.value,
-              },
-            },
-          }),
-        })
-      }
-      value={state.slides[0].data.statement.content}
-    />
-    
-    <button
-      className="btn btn-download"
-      onClick={() =>
-        html2image(
-          {
-            state,
-            setState,
-          },
-          `mitglieder-1-${state.slides[0].data.statement.content}`
-        )
-      }
-    >
-      Download
-    </button>
-  </>
-);
+      <Input
+        label="Titel"
+        state={state}
+        setState={setState}
+        propertyPath={`slides[${currentSlide}].data.statement.content`}
+      />
+
+      <DownloadButton
+        state={state}
+        setState={setState}
+        fileNamePath={`slides[${currentSlide}].data.statement.content`}
+      />
+    </>
+  );
+};
