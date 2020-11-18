e5e3aebb7fa923e5f8adac389871dac70cfe7447
Tilman Miraß <mail@tlmn.io>
2020-11-18 15:39:56 +0100

    make text italic

--- src/templates/politische-bildung/slide-0/template.jsx
+++ src/templates/politische-bildung/slide-0/template.jsx
@@ -22,7 +22,7 @@ export default ({ state, thumbnail = false }) => (
         />
         <div className="flex-1 flex">
           <span
-            className="block // w-full // self-center // text-left text-black font-bold font-headline uppercase leading-none"
+            className="block // w-full // self-center // text-left text-black font-bold font-headline uppercase italic leading-none"
             style={{
               fontSize: `${state.slides[0].data.body.scale.value}px`,
             }}
