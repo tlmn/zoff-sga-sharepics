cfd6c5031a74e772fb587b3a6fd484d4bc09d531
Tilman Miraß <mail@tlmn.io>
2021-06-16 16:20:31 +0200

    add template markup

--- src/templates/news/template.jsx
+++ src/templates/news/template.jsx
@@ -22,7 +22,7 @@ export default () => {
                 propertyPath={`slides[${state.currentSlide}].data.images.upper.position`}
               />
             </div>
-            {!state.slides[state.currentSlide].data.images.isOnlyOne && (
+            {!state.slides[state.currentSlide].data.images.onlyOneImage && (
               <div className="flex-1">
                 <DraggableBG
                   propertyPath={`slides[${state.currentSlide}].data.images.lower.position`}
@@ -31,7 +31,25 @@ export default () => {
             )}
           </div>
           <div className="w-full h-full relative">
-            <div className="flex flex-col w-full h-full bg-white absolute top-0 left-0">
+            <div
+              className={`flex flex-col w-full h-full absolute top-0 left-0 z-20`}
+            >
+              <div
+                className={`flex-1 flex ${
+                  state.slides[state.currentSlide].data.logo.options
+                    .position === "top-right" && `justify-end items-start`
+                } ${
+                  state.slides[state.currentSlide].data.logo.options
+                    .position === "bottom-center" && `justify-center items-end`
+                }`}
+              >
+                <LogoText
+                  fillColor="#fff"
+                  style={{ boxShadow: "0px 4px 4px 0px #00000040" }}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col w-full h-full absolute top-0 left-0">
               <div className="flex-1">
                 <div
                   className="relative // top-0 left-0 right-0 // z-10 // w-full h-full"
@@ -50,7 +68,7 @@ export default () => {
                   }}
                 />
               </div>
-              {!state.slides[state.currentSlide].data.images.isOnlyOne && (
+              {!state.slides[state.currentSlide].data.images.onlyOneImage && (
                 <div className="flex-1">
                   <div
                     className="relative // top-0 left-0 right-0 // z-10 // w-full h-full"
@@ -71,18 +89,32 @@ export default () => {
                 </div>
               )}
             </div>
-            <div className="relative z-10  w-full h-full flex items-center justify-center">
-              <div className="w-full bg-red py-3">
-                {state.slides[0].data.body.lines.map((line) => (
+            <div
+              className={`relative z-10  w-full h-full flex justify-center ${state.slides[0].data.body.options.position}`}
+            >
+              <div className="w-full">
+                <div className="w-full bg-red py-3">
+                  {state.slides[0].data.body.lines.map((line) => (
+                    <span
+                      className="block text-center font-sans font-bold"
+                      style={{
+                        fontSize: `${line.scale.value}px`,
+                      }}
+                    >
+                      {line.content}
+                    </span>
+                  ))}
+                </div>
+                <div className={`bg-black text-white`}>
                   <span
                     className="block text-center font-sans font-bold"
                     style={{
-                      fontSize: `${line.scale.value}px`,
+                      fontSize: `${state.slides[0].data.subline.scale.value}px`,
                     }}
                   >
-                    {line.content}
+                    {state.slides[0].data.subline.content}
                   </span>
-                ))}
+                </div>
               </div>
             </div>
           </div>
