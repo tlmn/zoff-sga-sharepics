4e15fec815839ab699a04d76609a56b221d359cb
Tilman Miraß <mail@tlmn.io>
2021-06-16 12:37:00 +0200

    make control elements more compact

--- src/components/inputs/bgImage.jsx
+++ src/components/inputs/bgImage.jsx
@@ -1,69 +1,70 @@
 import React, { useContext, useRef } from "react";
+import { getProperty, updateProperty } from "../../lib/lib";
 
 import IconReset from "../../assets/svg/reset";
 import TemplateContext from "../templateContext";
-import { updateProperty } from "../../lib/lib";
 
-export default ({ currentSlide }) => {
+export default ({ propertyPath, label = "Hintergrundbild" }) => {
   const [state, setState] = useContext(TemplateContext);
   const inputFileRef = useRef(null);
   return (
     <>
-      <label htmlFor="bgImage__fileName">Hintergrundbild</label>
-      <input
-        type="file"
-        id="bgImage__fileName"
-        name="file"
-        onChange={(e) =>
-          e.target.files[0] !== null &&
-          updateProperty(
-            { state, setState },
-            `slides[${currentSlide}].data.image.url`,
-            URL.createObjectURL(e.target.files[0])
-          )
-        }
-        ref={inputFileRef}
-        className="hidden"
-      />
+      <label htmlFor="bgImage__fileName">{label}</label>
+      <div class="flex">
+        <input
+          type="file"
+          id="bgImage__fileName"
+          name="file"
+          onChange={(e) =>
+            e.target.files[0] !== null &&
+            updateProperty(
+              { state, setState },
+              `${propertyPath}.url`,
+              URL.createObjectURL(e.target.files[0])
+            )
+          }
+          ref={inputFileRef}
+          className="hidden"
+        />
 
-      <button
-        type="button"
-        onClick={() => inputFileRef.current.click()}
-        className="btn flex justify-center mb-2"
-      >
-        Datei auswählen
-      </button>
+        <button
+          type="button"
+          onClick={() => inputFileRef.current.click()}
+          className="btn flex justify-center mb-2"
+        >
+          Datei auswählen
+        </button>
 
-      <label htmlFor="bgImage__scale">Zoomfaktor für Bild</label>
-      <input
-        type="range"
-        id="bgImage__scale"
-        name="imageScale"
-        min="0"
-        defaultValue={state.slides[currentSlide].data.image.scale}
-        max="30"
-        onChange={(e) =>
-          updateProperty(
-            { state, setState },
-            `slides[${currentSlide}].data.image.scale`,
-            e.target.value
-          )
-        }
-      />
+        <label htmlFor="bgImage__scale">Zoom</label>
+        <input
+          type="range"
+          id="bgImage__scale"
+          name="imageScale"
+          min="0"
+          defaultValue={getProperty({ state }, `${propertyPath}.scale`)}
+          max="30"
+          onChange={(e) =>
+            updateProperty(
+              { state, setState },
+              `${propertyPath}.scale`,
+              e.target.value
+            )
+          }
+        />
 
-      <button
-        className="btn flex justify-center mb-2"
-        onClick={() =>
-          updateProperty(
-            { state, setState },
-            `slides[${currentSlide}].data.image.position`,
-            { x: 0, y: 0 }
-          )
-        }
-        type="button"
-      >
-        <IconReset height="20" className="block mr-1" /> Bildausschnitt
-      </button>
+        <button
+          className="btn flex justify-center mb-2"
+          onClick={() =>
+            updateProperty({ state, setState }, `${propertyPath}.position`, {
+              x: 0,
+              y: 0,
+            })
+          }
+          type="button"
+        >
+          <IconReset height="20" className="block mr-1" />Ausschnitt
+        </button>
+      </div>
     </>
   );
 };
