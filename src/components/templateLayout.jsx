7755397a8c0131ea36a201657c6a76419f1fbeae
Tilman Miraß <mail@tlmn.io>
2021-07-09 15:39:19 +0200

    move header up

--- src/components/templateLayout.jsx
+++ src/components/templateLayout.jsx
@@ -4,16 +4,18 @@ import React from 'react'
 
 export default ({ children }) => (
   <Layout>
-    <div className="container grid-12">
-      <div className="col-span-12 py-2">
-        <Link
-          to="/"
-          className="hover:underline uppercase text-white font-kapra font-bold"
-        >
-          ◂ zurück zur Übersicht
-        </Link>
+    <div className="bg-red">
+      <div className="container grid-12 mb-2">
+        <div className="col-span-12 py-2">
+          <Link
+            to="/"
+            className="hover:underline uppercase text-white font-kapra font-bold"
+          >
+            ◂ zurück zur Übersicht
+          </Link>
+        </div>
       </div>
-      {children}
     </div>
+    <div className="container grid-12">{children}</div>
   </Layout>
 )
