8d727bb23153d3f6a4eefc2546534b062b1410e6
Tilman Miraß <mail@tlmn.io>
2021-07-09 15:38:58 +0200

    add bg.white

--- src/components/controlsWrapper.jsx
+++ src/components/controlsWrapper.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ControlsWrapper = ({ children }) => (
-  <div className="flex flex-col justify-center">{children}</div>
+  <div className="flex flex-col justify-center bg-white p-2">{children}</div>
 )
 
 export default ControlsWrapper
