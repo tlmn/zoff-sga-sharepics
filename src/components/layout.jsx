2463cb10d5f4ddcdf1a5f6191807d68421b17618
tlmn <mail@tlmn.io>
2021-07-17 09:41:33 +0200

    display Safari message

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,6 +2,7 @@ import { graphql, useStaticQuery } from 'gatsby'
 
 import { Helmet } from 'react-helmet'
 import React from 'react'
+import { isSafari } from 'react-device-detect'
 
 export default ({ children }) => {
   const {
@@ -33,7 +34,20 @@ export default ({ children }) => {
         <meta name="description" content={description} />
         <meta property="og:title" content={longTitle} />
       </Helmet>
-      <div className="hidden xl:block pb-2">{children}</div>
+      {!isSafari ? (
+        <div className="hidden xl:block pb-2">{children}</div>
+      ) : (
+        <div className="flex xl:hidden bg-red w-screen h-screen flex-col items-center justify-center px-3">
+          <h1 className="text-xs text-yellow leading-none font-bold font-ttnorms text-center">
+            Sharepic Generator <br />
+            Solidarisch geht anders
+          </h1>
+          <h2 className="text-2xs text-black leading-none font-ttnorms text-center mt-2">
+            Der Generator läuft leider nicht mit Safari. Bitte verwende Firefox
+            oder einen anderen Browser.
+          </h2>
+        </div>
+      )}
 
       <div className="flex xl:hidden bg-red w-screen h-screen flex-col items-center justify-center px-3">
         <h1 className="text-xs text-yellow leading-none font-bold font-ttnorms text-center">
