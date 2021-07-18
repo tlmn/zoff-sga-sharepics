86e63b39b66e60cedaaae0b4875268b49adef4d1
tlmn <mail@tlmn.io>
2021-07-18 21:11:31 +0200

    style Safari message

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -37,12 +37,12 @@ export default ({ children }) => {
       {!isSafari ? (
         <div className="hidden xl:block pb-2">{children}</div>
       ) : (
-        <div className="flex xl:hidden bg-red w-screen h-screen flex-col items-center justify-center px-3">
+        <div className="flex bg-red w-screen h-screen flex-col items-center justify-center px-3">
           <h1 className="text-xs text-yellow leading-none font-bold font-ttnorms text-center">
             Sharepic Generator <br />
             Solidarisch geht anders
           </h1>
-          <h2 className="text-2xs text-black leading-none font-ttnorms text-center mt-2">
+          <h2 className="text-2xs text-black leading-none font-ttnorms text-center mt-2 w-1/2">
             Der Generator läuft leider nicht mit Safari. Bitte verwende Firefox
             oder einen anderen Browser.
           </h2>
