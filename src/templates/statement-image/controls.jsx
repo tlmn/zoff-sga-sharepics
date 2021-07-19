e197c053638993345a5ea57ff8aa068f416b419a
tlmn <mail@tlmn.io>
2021-07-19 10:07:17 +0200

    add legend to image controls

--- src/templates/statement-image/controls.jsx
+++ src/templates/statement-image/controls.jsx
@@ -30,7 +30,7 @@ export default () => {
         />
       </FieldSet>
 
-      <FieldSet>
+      <FieldSet legend="Bild">
         <Image propertyPath={`slides[${currentSlide}].data.image`} />
       </FieldSet>
 
